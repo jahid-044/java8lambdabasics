43a1c404cd558ea98619f44d6c8cb6b6e3e39a17
jhasan <jahid.hasan@dsinnovators.com>
2024-01-23 19:02:24 +0600

    Adding basic lambda8 java project for learning purpose.

---
+++ src/io/javabrains/Greeter.java
@@ -0,0 +1,14 @@
+package io.javabrains;
+
+public class Greeter {
+    public void greet(Greeting greeting) {
+//        System.out.print("Hello World!");
+        greeting.perform();
+    }
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
+        greeter.greet(helloWorldGreeting);
+    }
+}
