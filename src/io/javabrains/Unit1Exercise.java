6f1d7cfd09795f5bc4f71b66e3113e52fd1a2122
jhasan <jahid.hasan@dsinnovators.com>
2024-02-29 20:59:17 +0600

    Added Person and Unit1Exercise class

---
+++ src/io/javabrains/Unit1Exercise.java
@@ -0,0 +1,17 @@
+package io.javabrains;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Unit1Exercise {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Chales","Dickens",60),
+                new Person("Lewis","Carroll",42),
+                new Person("Thomas","Carlyle",51),
+                new Person("Matthew","Arnold",39)
+        );
+
+    }
+}
